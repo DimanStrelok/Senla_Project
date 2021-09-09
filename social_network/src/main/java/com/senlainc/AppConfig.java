2688bb4591855a81ab6c29fab2bc3eabe9671e1a
Dmitry Astapov <astapov.dima1999@gmail.com>
2021-09-09 12:05:45 +0300

    social network: add docker and docker-compose

--- social_network/src/main/java/com/senlainc/AppConfig.java
+++ social_network/src/main/java/com/senlainc/AppConfig.java
@@ -31,7 +31,7 @@ public class AppConfig {
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/postgres");
+        dataSource.setUrl("jdbc:postgresql://postgres:5432/postgres");
         dataSource.setUsername("postgres");
         dataSource.setPassword("postgres");
         return dataSource;
